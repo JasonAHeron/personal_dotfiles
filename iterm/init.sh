a11789de560840b0fe0892177c940f69ec1f3344
Jason Heron <jason@heron.io>
2016-04-30 21:01:01 -0700

    adding osx dotfile

---
+++ iterm/init.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+/usr/libexec/PlistBuddy -c 'Set :"New Bookmarks":0:"Non Ascii Font" MesloLGMDZ-RegularForPowerline 12' ~/Library/Preferences/com.googlecode.iterm2.plist
+/usr/libexec/PlistBuddy -c 'Set :"New Bookmarks":0:"Normal Font" MesloLGMDZ-RegularForPowerline 12' ~/Library/Preferences/com.googlecode.iterm2.plist
